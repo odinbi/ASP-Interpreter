fa1b26b224bb5ada395dcbcdabd8ef810f66f3d4
Thomas Gulli <thomas_gulli@hotmail.com>
2017-09-11 19:19:28 +0200

    Tried to fixed float problem, closer than before :P

--- prekode/no/uio/ifi/asp/scanner/Scanner.java
+++ prekode/no/uio/ifi/asp/scanner/Scanner.java
@@ -87,263 +87,263 @@ public class Scanner {
 
         //-- Must be changed in part 1:zzz
         if(line == null){ //  || line == ""
-            System.out.println("End of file");
-            curLineTokens.add(new Token(eofToken, curLineNum()));
-            for (Token t: curLineTokens)
-                Main.log.noteToken(t);
-            return;
-        } else if(ignoreLine(line)){
-            readNextLine();
-            return;
-        } else{
-            System.out.println("In readNextLine - parse: " + line);
-            String stringLine = expandLeadingTabs(line);
-            int countBlank = countBlanks(stringLine.toCharArray());
-            indentHandle(countBlank);
-            String word = "init";
-            while(word != "" && word != "\n"){
-                word = getWord(line);
-                System.out.println("In readNextLine - word fetched: " + word);
-                handleWord(word);
-                System.out.println("In readNextLine - word handleled: " + word);
-            }
-        }
-
-        // Add all tokens to log:
+        System.out.println("End of file");
+        curLineTokens.add(new Token(eofToken, curLineNum()));
         for (Token t: curLineTokens)
-            Main.log.noteToken(t);
-        //Get all tokens on next line
+        Main.log.noteToken(t);
+        return;
+    } else if(ignoreLine(line)){
         readNextLine();
         return;
-    }
-
-    private void handleWord(String word){
-        Token t = null;
-        if(isKeyword(word) || isDelimiter(word) || isOperator(word)){
-            t = new Token(TokenKind.getTokenKind(word), curLineNum());
-        } else if(word.startsWith("\"") && word.endsWith("\"")){
-            t = new Token(stringToken, curLineNum());
-            t.stringLit = word.substring(1, word.length()-1);
-        } else if(word.matches("\\d*")){
-            try{
-                int n = Integer.parseInt(word);
-                t = new Token(integerToken, curLineNum());
-                t.integerLit = n;
-            } catch(NumberFormatException e1){
-                try {
-                    float f = Float.parseFloat(word);
-                    t = new Token(floatToken, curLineNum());
-                    t.floatLit = f;
-                } catch(NumberFormatException e2){
-                    Main.error(e2.getMessage());
-                }
-            }
-        } else if(word.matches("\\d*\\.?\\d*")){
-            t = new Token(floatToken, curLineNum());
-            t.floatLit = Float.parseFloat(word);
-        } else if(word == "\n" || word == "\r"){
-            t = new Token(newLineToken, curLineNum());
-        } else if(word != null && word != "" && word != " "){
-            t = new Token(nameToken, curLineNum());
-            t.name = word;
-        } else {
-            Main.error("handleWord error! Could not recognize word: " + word + ", at line: " + curLineNum());
+    } else{
+        System.out.println("In readNextLine - parse: " + line);
+        String stringLine = expandLeadingTabs(line);
+        int countBlank = countBlanks(stringLine.toCharArray());
+        indentHandle(countBlank);
+        String word = "init";
+        while(word != "" && word != "\n"){
+            word = getWord(line);
+            System.out.println("In readNextLine - word fetched: " + word);
+            handleWord(word);
+            System.out.println("In readNextLine - word handleled: " + word);
         }
-        curLineTokens.add(t);
     }
 
-    private String getWord(String stringLine){
-        String curWord = "";
-        boolean inString = false;
+    // Add all tokens to log:
+    for (Token t: curLineTokens)
+    Main.log.noteToken(t);
+    //Get all tokens on next line
+    readNextLine();
+    return;
+}
 
-        if (stringLine.length() <= currentIndex) return "\n";
-
-        for (int index = currentIndex; index < stringLine.length(); ++index){
-
-            char currentChar = stringLine.charAt(index);
-
-            //System.out.println("word: \"" + curWord + "\" char: \'" + currentChar + "\'");
-            //System.out.println("input length: " + stringLine.length() + " index: " + index);
-            if(inString){
-                if(currentChar == '\"'){ // '\"' = 34
-                    curWord += currentChar;
-                    currentIndex = ++index;
-                    return curWord;
-                }
-                curWord += currentChar;
-            } else if(isOperator(String.valueOf(currentChar)) || isDelimiter(String.valueOf(currentChar))){
-                System.out.println("opperator/delimiter reached: " + currentChar);
-                if(curWord != ""){
-                    System.out.println("Is this an opperator? " + curWord + currentChar);
-                    if(isDelimiter(curWord + currentChar) || isOperator(curWord + currentChar)){
-                        curWord += currentChar;
-                    } else{
-                        currentIndex = index;
-                        return curWord;
-                    }
-                } else {
-                    //currentIndex = index;
-                    curWord += currentChar;
-                }
-            } else if(isDelimiter(curWord) || isOperator(curWord)){
-                currentIndex = index;
-                return  curWord;
-            } else {
-                if(currentChar == ' '){
-                    currentIndex = index;
-                    if(curWord != "") return curWord;
-                } else if(currentChar == '\"'){ // '\"' = 34 ascii character
-                    if(curWord == ""){
-                        inString = true;
-                        curWord += currentChar;
-                    } else{
-                        currentIndex = index;
-                        return curWord;
-                    }
-                } else{
-                    curWord += currentChar;
-                }
+private void handleWord(String word){
+    Token t = null;
+    System.out.println("Ord: " + word);
+    if(isKeyword(word) || isDelimiter(word) || isOperator(word)){
+        t = new Token(TokenKind.getTokenKind(word), curLineNum());
+    } else if(word.startsWith("\"") && word.endsWith("\"")){
+        t = new Token(stringToken, curLineNum());
+        t.stringLit = word.substring(1, word.length()-1);
+    } else if(word.matches("\\d*")){
+        try{
+            int n = Integer.parseInt(word);
+            t = new Token(integerToken, curLineNum());
+            t.integerLit = n;
+        } catch(NumberFormatException e1){
+            try {
+                float f = Float.parseFloat(word);
+                t = new Token(floatToken, curLineNum());
+                t.floatLit = f;
+            } catch(NumberFormatException e2){
+                Main.error(e2.getMessage());
             }
-            if(index == stringLine.length()-1){
-                System.out.println("returns: " + curWord);
-                currentIndex = ++index;
+        }
+    } else if(word.matches("\\d*\\.?\\d*")){
+        t = new Token(floatToken, curLineNum());
+        t.floatLit = Float.parseFloat(word);
+    } else if(word == "\n" || word == "\r"){
+        t = new Token(newLineToken, curLineNum());
+    } else if(word != null && word != "" && word != " "){
+        t = new Token(nameToken, curLineNum());
+        t.name = word;
+    } else {
+        Main.error("handleWord error! Could not recognize word: " + word + ", at line: " + curLineNum());
+    }
+    curLineTokens.add(t);
+}
+
+private String getWord(String stringLine){
+    String curWord = "";
+    boolean inString = false;
+
+    if (stringLine.length() <= currentIndex) return "\n";
+
+    for (int index = currentIndex; index < stringLine.length(); ++index){
+
+        char currentChar = stringLine.charAt(index);
+
+        //System.out.println("word: \"" + curWord + "\" char: \'" + currentChar + "\'");
+        //System.out.println("input length: " + stringLine.length() + " index: " + index);
+        if(inString){
+            if(currentChar == '\"'){ // '\"' = 34
+            curWord += currentChar;
+            currentIndex = ++index;
+            return curWord;
+        }
+        curWord += currentChar;
+    } else if((curWord + currentChar).matches("\\d*\\.\\d*")){
+        curWord += currentChar;
+    } else if(isOperator(String.valueOf(currentChar)) || isDelimiter(String.valueOf(currentChar))){
+        System.out.println("opperator/delimiter reached: " + currentChar);
+        if(curWord != ""){
+            System.out.println("Is this an opperator? " + curWord + currentChar);
+            if(isDelimiter(curWord + currentChar) || isOperator(curWord + currentChar)){
+                curWord += currentChar;
+            } else{
+                currentIndex = index;
                 return curWord;
             }
+        } else {
+            //currentIndex = index;
+            curWord += currentChar;
         }
-        return curWord;
-    }
-
-    private boolean isKeyword(String word){
-        String[] keyArray = {"and", "as", "assert", "break", "class", "continue", "def", "del", "elif", "else",
-        "except", "False", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "None",
-        "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield"};
-        return Arrays.asList(keyArray).contains(word);
-    }
-
-    private boolean isOperator(String word){
-        String[] operatorsArray = {"&", "*", "|", "**", "==", "<<", ">>", "//", "<",
-        ">", "<=", ">=", "^", "-", "!=", "%", "+", "/", "~"};
-        return Arrays.asList(operatorsArray).contains(word);
-    }
-
-    private boolean isDelimiter(String word){
-        String[] delimitersArray = {"&=", "*=", "@", "|=", ":", ",", ".", "**==",
-        "<<=", ">>=", "//=", "=", "^=", "{", "[", "(", "-=",
-        "%=", "+=", "}", "]", ")", ";", "/="};
-        return Arrays.asList(delimitersArray).contains(word);
-    }
-
-
-    public int curLineNum() {
-        return sourceFile!=null ? sourceFile.getLineNumber() : 0;
-    }
-
-    private int findIndent(String s) {
-        int indent = 0;
-
-        while (indent<s.length() && s.charAt(indent)==' ') indent++;
-        return indent;
-    }
-
-    //Pattern commentLine = Pattern.compile("^\\s*#");
-    //Pattern blankLine = Pattern.compile("^\\s*$");
-
-
-    public boolean ignoreLine(String input){
-        boolean cmtL = input.matches("^\\s*#.*");
-        boolean blnkL = input.matches("^\\s*$");
-        return (cmtL || blnkL);
-    }
-
-    private void indentsPush(int i){
-        indents[numIndents] = i;
-        curLineTokens.add(new Token(indentToken, curLineNum()));
-        numIndents++;
-    }
-    private void indentsPop(){
-        curLineTokens.add(new Token(dedentToken, curLineNum()));
-        numIndents--;
-    }
-    private int indentsTop(){
-        return indents[numIndents-1];
-    }
-
-    private void indentHandle(int blanks){
-        if(indentsTop() < blanks){
-            indentsPush(blanks);
-            //add token INDENT
+    } else if(isDelimiter(curWord) || isOperator(curWord)){
+        currentIndex = index;
+        return  curWord;
+    } else {
+        if(currentChar == ' '){
+            currentIndex = index;
+            if(curWord != "") return curWord;
+        } else if(currentChar == '\"'){ // '\"' = 34 ascii character
+        if(curWord == ""){
+            inString = true;
+            curWord += currentChar;
+        } else{
+            currentIndex = index;
+            return curWord;
         }
-        else if(indentsTop() > blanks){
-            indentsPop();
-            //add token DEDENT
+    } else{
+        curWord += currentChar;
+    }
+    System.out.println("CurWord" + curWord);
+}
+if(index == stringLine.length()-1){
+    System.out.println("returns: " + curWord);
+    currentIndex = ++index;
+    return curWord;
+}
+}
+return curWord;
+}
+
+private boolean isKeyword(String word){
+    String[] keyArray = {"and", "as", "assert", "break", "class", "continue", "def", "del", "elif", "else",
+    "except", "False", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "None",
+    "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield"};
+    return Arrays.asList(keyArray).contains(word);
+}
+
+private boolean isOperator(String word){
+    String[] operatorsArray = {"&", "*", "|", "**", "==", "<<", ">>", "//", "<",
+    ">", "<=", ">=", "^", "-", "!=", "%", "+", "/", "~"};
+    return Arrays.asList(operatorsArray).contains(word);
+}
+
+private boolean isDelimiter(String word){
+    String[] delimitersArray = {"&=", "*=", "@", "|=", ":", ",", ".", "**==",
+    "<<=", ">>=", "//=", "=", "^=", "{", "[", "(", "-=",
+    "%=", "+=", "}", "]", ")", ";", "/="};
+    return Arrays.asList(delimitersArray).contains(word);
+}
+
+
+public int curLineNum() {
+    return sourceFile!=null ? sourceFile.getLineNumber() : 0;
+}
+
+private int findIndent(String s) {
+    int indent = 0;
+
+    while (indent<s.length() && s.charAt(indent)==' ') indent++;
+    return indent;
+}
+
+public boolean ignoreLine(String input){
+    boolean cmtL = input.matches("^\\s*#.*");
+    boolean blnkL = input.matches("^\\s*$");
+    return (cmtL || blnkL);
+}
+
+private void indentsPush(int i){
+    indents[numIndents] = i;
+    curLineTokens.add(new Token(indentToken, curLineNum()));
+    numIndents++;
+}
+private void indentsPop(){
+    curLineTokens.add(new Token(dedentToken, curLineNum()));
+    numIndents--;
+}
+private int indentsTop(){
+    return indents[numIndents-1];
+}
+
+private void indentHandle(int blanks){
+    if(indentsTop() < blanks){
+        indentsPush(blanks);
+        //add token INDENT
+    }
+    else if(indentsTop() > blanks){
+        indentsPop();
+        //add token DEDENT
+    }
+}
+
+private int countBlanks(char[] line){
+    int numberOfBlanks = 0;
+    for (int i = 0; i < line.length; i++){
+        if (line[i] == ' '){
+            numberOfBlanks++;
+        } else{
+            return numberOfBlanks;
         }
     }
+    return -1;
+}
 
-    private int countBlanks(char[] line){
-        int numberOfBlanks = 0;
-        for (int i = 0; i < line.length; i++){
-            if (line[i] == ' '){
-                numberOfBlanks++;
-            } else{
-                return numberOfBlanks;
-            }
-        }
-        return -1;
-    }
-
-    private String expandLeadingTabs(String s) {
-        String newS = "";
-        for (int i = 0;  i < s.length();  i++) {
-            char c = s.charAt(i);
-            if (c == '\t') {
-                do {
-                    newS += " ";
-                } while (newS.length()%tabDist != 0);
-            } else if (c == ' ') {
+private String expandLeadingTabs(String s) {
+    String newS = "";
+    for (int i = 0;  i < s.length();  i++) {
+        char c = s.charAt(i);
+        if (c == '\t') {
+            do {
                 newS += " ";
-            } else {
-                newS += s.substring(i);
-                break;
-            }
+            } while (newS.length()%tabDist != 0);
+        } else if (c == ' ') {
+            newS += " ";
+        } else {
+            newS += s.substring(i);
+            break;
         }
-        return newS;
     }
+    return newS;
+}
 
 
-    private boolean isLetterAZ(char c) {
-        return ('A'<=c && c<='Z') || ('a'<=c && c<='z') || (c=='_');
-    }
+private boolean isLetterAZ(char c) {
+    return ('A'<=c && c<='Z') || ('a'<=c && c<='z') || (c=='_');
+}
 
 
-    private boolean isDigit(char c) {
-        return '0'<=c && c<='9';
-    }
+private boolean isDigit(char c) {
+    return '0'<=c && c<='9';
+}
 
 
-    public boolean isCompOpr() {
-        TokenKind k = curToken().kind;
-        //-- Must be changed in part 2:
-        return false;
-    }
+public boolean isCompOpr() {
+    TokenKind k = curToken().kind;
+    //-- Must be changed in part 2:
+    return false;
+}
 
 
-    public boolean isFactorPrefix() {
-        TokenKind k = curToken().kind;
-        //-- Must be changed in part 2:
-        return false;
-    }
+public boolean isFactorPrefix() {
+    TokenKind k = curToken().kind;
+    //-- Must be changed in part 2:
+    return false;
+}
 
 
-    public boolean isFactorOpr() {
-        TokenKind k = curToken().kind;
-        //-- Must be changed in part 2:
-        return false;
-    }
+public boolean isFactorOpr() {
+    TokenKind k = curToken().kind;
+    //-- Must be changed in part 2:
+    return false;
+}
 
 
-    public boolean isTermOpr() {
-        TokenKind k = curToken().kind;
-        //-- Must be changed in part 2:
-        return false;
-    }
+public boolean isTermOpr() {
+    TokenKind k = curToken().kind;
+    //-- Must be changed in part 2:
+    return false;
+}
 }
